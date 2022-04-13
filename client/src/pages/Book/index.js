a734147c3fcc80980d7cb17be864e3521058f9c5
Joaomgsa <joaomgsa@gmail.com>
2022-04-13 00:02:50 -0300

    cabecalho pagina livros

--- client/src/pages/Book/index.js
+++ client/src/pages/Book/index.js
@@ -1,17 +1,18 @@
-import react from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import {FiPower} from 'react-icons/fi';
 
 import './styles.css';
+
 import logoImage from '../../assets/logo.svg';
 
 export default function Book() {
     return (
         <div className="book-container">
             <header>
-                <img src="{logoImage}" alt="Erudio" />
+                <img src={logoImage} alt="Erudio" />
                 <span>Bem vindo, <strong>João</strong>!</span>
-                <link className="button" to="book/new">Adicionar novo Livro</link>
+                <Link className="button" to="book/new">Adicionar novo Livro</Link>
                 <button type="button">
                     <FiPower size={18} color="#251fc5"/>
                 </button>
