700abdb7b50d5cf2b228b23b54facf8ace1331c5
Joaomgsa <joaomgsa@gmail.com>
2022-04-15 22:54:29 -0300

    ajuste link novo llivro

--- client/src/pages/Books/index.js
+++ client/src/pages/Books/index.js
@@ -12,7 +12,7 @@ export default function Books() {
             <header>
                 <img src={logoImage} alt="Erudio" />
                 <span>Bem vindo, <strong>João</strong>!</span>
-                <Link className="button" to="book/new">Adicionar novo Livro</Link>
+                <Link className="button" to="./new">Adicionar novo Livro</Link>
                 <button type="button">
                     <FiPower size={18} color="#251fc5"/>
                 </button>
