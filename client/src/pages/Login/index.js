3a142cae30f5fcaab771578351b6b37281570a18
Joaomgsa <joaomgsa@gmail.com>
2022-04-16 00:03:02 -0300

    axios login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,20 +1,54 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
 import './styles.css';
+
+import api from "../../services/api";
 
 import logoImage from '../../assets/logo.svg';
 import padlock from '../../assets/padlock.png';
 
 
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function login (e) {
+        e.preventDefault();
+
+        const data = {
+            username,
+            password,
+        };
+
+        try {
+
+            const response = await api.post('auth/signin',data)
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.token);
+            
+        } catch (error) {
+            alert('Falha no Login! Tente novamente');  
+        }
+
+
+    };
+
     return (
            <div className="login-container">
                <section className="form">
                <img src={logoImage} alt="Logo"/>
-               <form>
+               <form onSubmit={login}>
                    <h1>Access Your Account</h1>
-                   <input placeholder="Username"/>
-                   <input type="password" placeholder="Password"/>
+                   <input placeholder="Username"
+                          value={username}
+                          onChange={e => setUsername(e.target.value)}/>
+                   <input type="password" placeholder="Password"
+                         value={password}
+                         onChange={e => setPassword(e.target.value)}/>
 
                    <button className="button" type="submit">Login</button>
                </form>
